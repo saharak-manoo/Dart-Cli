7dc26d847038db90325e8a16ee6ebff3ea41b1fd
Saharak Manoo <Saharakmanoo@outlook.com>
2020-04-12 13:08:33 +0700

    [Updated] array

--- main.dart
+++ main.dart
@@ -1,9 +1,11 @@
+import 'dart:math';
+
 class Bicycle {
   int cadence;
   int gear;
   int _speed = 99;
 
-  Bicycle(this.cadence, this.gear);
+  Bicycle(this.cadence, {this.gear = 1});
   int get speed => _speed;
   String info() {
     return 'This bike has: ${cadence.toString()} cadence, ${gear.toString()} gear, and ${_speed.toString()} speed.';
@@ -13,7 +15,45 @@ class Bicycle {
   // String toString() => 'Bicycle: $speed mph';
 }
 
+abstract class Shape {
+  num get area;
+}
+
+class Circle implements Shape {
+  final num radius;
+  Circle(this.radius);
+  num get area => pi * pow(radius, 2);
+}
+
+class Square implements Shape {
+  final num side;
+  Square(this.side);
+  num get area => pow(side, 2);
+}
+
+Shape shapeFactory(String type) {
+  if (type == 'circle') return Circle(2);
+  if (type == 'square') return Square(2);
+  throw 'Can\'t create $type.';
+}
+
+String scream(int length) => "A${'a' * length}h!";
+
 void main() {
-  var bike = new Bicycle(2, 2);
-  print(bike.info());
+  final bike1 = new Bicycle(2, gear: 9);
+  final bike2 = new Bicycle(4);
+  print(bike1.info());
+  print(bike2.info());
+
+  final circle = shapeFactory('circle');
+  final square = shapeFactory('square');
+  print(circle.area);
+  print(square.area);
+
+  // Array
+  final values = [1, 2, 3, 5, 10, 50];
+  values.map(scream).forEach(print);
+
+  // Skip array
+  values.skip(1).take(3).map(scream).forEach(print);
 }
